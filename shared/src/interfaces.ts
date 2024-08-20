7c5952653d264dd7d9317fe8d841bf42c9820363
ghulamabbas2 <ghulamabbas258@gmail.com>
2024-08-20 17:24:16 +0500

    authentication, login, signup with validations

--- shared/src/interfaces.ts
+++ shared/src/interfaces.ts
@@ -14,6 +14,25 @@ export const CarTransmissions = ["Automatic", "Manual"];
 export const CarDoors = [2, 4];
 export const CarSeats = [2, 4, 5, 7, 8, 9, 10];
 
+export const UserRoles = ["user", "admin"];
+
+export interface IUser {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  phoneNo: string;
+  role?: string[];
+  avatar?: {
+    url: string;
+    public_id: string;
+  };
+  resetPasswordToken: string | undefined;
+  resetPasswordExpire: Date | undefined;
+  createdAt: string;
+  updatedAt: string;
+}
+
 export interface ICar {
   id: string;
   name: string;
